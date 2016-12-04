269eb70e710f40cea50b33b4c9d77e78afaea2c9
praveshkoirala@gmail.com <praveshkoirala@gmail.com>
2016-12-04 22:58:56 +0545

    Fixes #1

--- content_script/script.js
+++ content_script/script.js
@@ -17,11 +17,12 @@ var img = ["http://i.imgur.com/JdNU6Bb.jpg",
 var hide = true;
 var replaceText = "I am an idiot."
 var replaceTexts = [
-	"I'm an idiot.",
+	"Clearly, I'm an idiot.",
 	"Oh god, why was I even born?",
 	"I must have hit my head quite hard sometime in my childhood.",
 	"Everyone, I apologize for my stupidity.",
 	"हल्का स्लो टाइपको छु, कृपया माइन्ड नगर्नुहोला।",
+	"बच्चामा च्यवनप्राश नखाएकोले बोद्धिक विकास हुन सकेन हजुर।"
 
 
 ]
@@ -42,14 +43,16 @@ function randomText(){
 function hideDot(hide, start,end){
   for(i=start; i<end; i++){
     var x = document.getElementsByClassName('UFICommentBody')[i].querySelector("span");
-    var comment = x.innerHTML;
-    if(comment == '.' || comment == ',' || comment == '*' || comment == "thoplo" || comment == "dot" || comment == ".." || comment == "..." || comment == "...."|| comment == "....."){
-      //if(hide) {
-        x.innerHTML = randomText();
-      //} else{
-      //  x.innerHTML = text
-      //}
-    }
+    if (x !== null){
+	    var comment = x.innerHTML;
+	    if(comment == '.' || comment == ',' || comment == '*' || comment == "thoplo" || comment == "dot" || comment == ".." || comment == "..." || comment == "...."|| comment == "....."){
+	      //if(hide) {
+	        x.innerHTML = randomText();
+	      //} else{
+	      //  x.innerHTML = text
+	      //}
+	    }
+	}
   }
 }
 
@@ -58,14 +61,13 @@ window.onload = function() {
   //document.getElementById("replaceText").value = replaceText;
   //document.getElementById("hide").checked = hide;
 
-  window.onscroll = function () {
-    // if (document.body.scrollTop > 30) {
-      //addArt();
-      var start = end;
-      end = document.getElementsByClassName('UFICommentBody').length;
-      hideDot(hide, start, end);
-    // }
-  };
+  // window.onscroll = function () {
+  //   // if (document.body.scrollTop > 30) {
+  //     //addArt();
+  //     var end = document.getElementsByClassName('UFICommentBody').length;
+		// hideDot(hide, 0, end);
+  //   // }
+  // };
 
   //document.getElementById("hide").onclick = function(){
   //  var hide = $("#hide:checked").val();
